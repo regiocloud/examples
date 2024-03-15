edcececdb9ad7463754c1a86471630e55af1c64b
Christian Berendt <berendt@osism.tech>
2024-03-15 13:48:08 +0100

    Fix hosts in the openstack ansible plays (#19)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- openstack/ansible/destroy.sh
+++ openstack/ansible/destroy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v $(pwd):/src:ro --rm -it ansible ansible-playbook -i localhost, destroy.yml
+docker run -v $(pwd):/src:ro --rm -it ansible ansible-playbook -c local -i localhost, destroy.yml
