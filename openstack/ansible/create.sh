d0525315011544d182edc8b296b5d7db2a841fd3
Christian Berendt <berendt@osism.tech>
2024-03-15 10:29:15 +0100

    Do not generate the ssh keypair inside the ansible container
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- openstack/ansible/create.sh
+++ openstack/ansible/create.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+if [[ ! -e id_rsa.test ]]; then
+    ssh-keygen -f id_rsa.test
+fi
+
 docker run -v $(pwd):/src:ro --rm -it ansible ansible-playbook -i localhost, playbook-create.yml
