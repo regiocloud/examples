06c6e10512beea4c3ba6c6d259fb1309f9a65985
Christian Berendt <berendt@osism.tech>
2024-03-15 13:28:49 +0100

    zuul: run openstack ansible example
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- openstack/ansible/create.sh
+++ openstack/ansible/create.sh
@@ -4,4 +4,4 @@ if [[ ! -e id_rsa.test ]]; then
     ssh-keygen -f id_rsa.test
 fi
 
-docker run -v $(pwd):/src:ro --rm -it ansible ansible-playbook -i localhost, playbook-create.yml
+docker run -v $(pwd):/src:ro --rm -it ansible ansible-playbook -i localhost, create.yml
--- openstack/ansible/destroy.sh
+++ openstack/ansible/destroy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v $(pwd):/src:ro --rm -it ansible ansible-playbook -i localhost, playbook-destroy.yml
+docker run -v $(pwd):/src:ro --rm -it ansible ansible-playbook -i localhost, destroy.yml
